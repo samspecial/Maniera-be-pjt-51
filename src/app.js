e1fd348b4e90a0333e4985d695ed1612bfd7afe7
Samuel Osinloye <psalmueloye@gmail.com>
2021-07-11 06:19:38 +0100

    product deployment

--- src/app.js
+++ src/app.js
@@ -22,9 +22,12 @@ dbSetup();
 
 // middlewares
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorizarion');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorizarion",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
